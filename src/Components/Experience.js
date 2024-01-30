a6d5668e4dc4cd4e7ff3da186ff9446c0f4c3464
Meirunas Smitas <meirunas@targech.com>
2024-01-30 11:32:20 +0000

    feat: add companies links

--- src/Components/Experience.js
+++ src/Components/Experience.js
@@ -21,7 +21,7 @@ const Experience = ({ data }) => {
     var work = data.work.map(function (work) {
       return (
         <div key={work.company}>
-          <h3>{work.company}</h3>
+          <h3><a href={work.link}>{work.company}</a></h3>
           <p className="info">
             {work.title}
             <span>&bull;</span> <em className="date">{work.years}</em>
